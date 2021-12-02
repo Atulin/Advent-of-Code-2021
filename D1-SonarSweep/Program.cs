2712f954c2888faca330583f5b4b54ad52128fb3
Angius <koumarin@gmail.com>
2021-12-02 02:06:32 +0100

    Advent of Code 2021 — day 1

---
+++ D1-SonarSweep/Program.cs
@@ -0,0 +1,10 @@
+﻿var lines = await File.ReadAllLinesAsync("./input.txt");
+var numbers = lines.Select(int.Parse).ToArray();
+
+var count = 0;
+for (var i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] > numbers[i - 1]) count++;
+}
+
+Console.WriteLine($"There are {count} values higher than previous values");
---
+++ D1B-SlidingSonarSweep/Program.cs
@@ -0,0 +1,15 @@
+﻿var lines = await File.ReadAllLinesAsync("./input.txt");
+var numbers = lines.Select(int.Parse).ToArray();
+
+var count = 0;
+for (var i = 3; i < numbers.Length; i++)
+{
+    var a = numbers[(i - 3)..i];
+    var b = numbers[(i - 2)..(i + 1)];
+
+    Console.WriteLine($"a: {string.Join(',', a)}, b: {string.Join(',', b)}");
+    
+    if (b.Sum() > a.Sum()) count++;
+}
+
+Console.WriteLine($"There are {count} values higher than previous values");
