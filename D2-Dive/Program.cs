df49f1871b0c2d6f3477c0d6a9046209971a2b23
Angius <koumarin@gmail.com>
2021-12-02 09:02:45 +0100

    Day 2

---
+++ D2-Dive/Program.cs
@@ -0,0 +1,24 @@
+﻿var lines = await File.ReadAllLinesAsync("./input.txt");
+
+var distance = 0;
+var depth = 0;
+
+foreach (var line in lines)
+{
+    var split = line.Split(' ');
+    var value = int.Parse(split[1]);
+    switch (split[0])
+    {
+        case "up":
+            depth -= value;
+            break;
+        case "down":
+            depth += value;
+            break;
+        case "forward":
+            distance += value;
+            break;
+    }
+}
+
+Console.WriteLine($"Depth of {depth} and distance of {distance} gives us {depth * distance}");
---
+++ D2B-DiveWithAim/Program.cs
@@ -0,0 +1,26 @@
+﻿var lines = await File.ReadAllLinesAsync("./input.txt");
+
+var distance = 0;
+var aim = 0;
+var depth = 0;
+
+foreach (var line in lines)
+{
+    var split = line.Split(' ');
+    var value = int.Parse(split[1]);
+    switch (split[0])
+    {
+        case "up":
+            aim -= value;
+            break;
+        case "down":
+            aim += value;
+            break;
+        case "forward":
+            distance += value;
+            depth += value * aim;
+            break;
+    }
+}
+
+Console.WriteLine($"Depth of {depth} and distance of {distance} gives us {depth * distance}");
