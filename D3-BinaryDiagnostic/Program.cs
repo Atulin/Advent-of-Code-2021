d2fed5d17bb3b39f195501009d34c0559f9ee588
Angius <koumarin@gmail.com>
2021-12-03 19:52:19 +0100

    Day 3

---
+++ D3-BinaryDiagnostic/Program.cs
@@ -0,0 +1,40 @@
+﻿var lines = await File.ReadAllLinesAsync("./input.txt");
+
+var counts = new Values[lines[0].Length].Select(_ => new Values()).ToArray();
+
+foreach (var l in lines)
+{
+    for (var i = 0; i < l.Length; i++)
+    {
+        var ch = l[i];
+        switch (ch)
+        {
+            case '0':
+                counts[i].Zeroes++;
+                break;
+            case '1':
+                counts[i].Ones++;
+                break;
+        }
+    }
+}
+
+Console.WriteLine("Counts are as follows:");
+Console.WriteLine(string.Join("\n", counts.Select(c => c.ToString())));
+
+var gamma = string.Join("", counts.Select(c => c.Ones > c.Zeroes ? '1' : '0'));
+var epsilon = string.Join("", counts.Select(c => c.Ones > c.Zeroes ? '0' : '1'));
+
+Console.WriteLine($"The gamma is {gamma} and epsilon is {epsilon}");
+
+var result = Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2);
+
+Console.WriteLine($"And the result is {result}");
+
+class Values
+{
+    public int Zeroes { get; set; }
+    public int Ones { get; set; }
+
+    public override string ToString() => $"(Zeroes: {Zeroes}, Ones: {Ones})";
+}
---
+++ D3B-BinaryDiagnosticO2CO2/Program.cs
@@ -0,0 +1,53 @@
+﻿var lines = await File.ReadAllLinesAsync("./input.txt");
+var lineLength = lines[0].Length;
+
+
+// Calculate values for oxygen
+var oxygenList = lines.ToList();
+for (var i = 0; i < lineLength; i++)
+{
+    if (oxygenList.Count == 1) break;
+    var values = CountAtLocation(oxygenList, i);
+    _ = oxygenList.RemoveAll(x => values.Zeroes > values.Ones ? x[i] == '1' : x[i] == '0');
+}
+
+var oxygenValue = Convert.ToInt32(oxygenList[0], 2);
+Console.WriteLine($"Oxygen value is {oxygenList[0]} or {oxygenValue}");
+
+
+// Calculate values for carbon dioxide
+var carbonDioxideList = lines.ToList();
+for (var i = 0; i < lineLength; i++)
+{
+    if (carbonDioxideList.Count == 1) break;
+    var values = CountAtLocation(carbonDioxideList, i);
+    _ = carbonDioxideList.RemoveAll(x => values.Zeroes <= values.Ones ? x[i] == '1' : x[i] == '0');
+}
+
+var carbonDioxideValue = Convert.ToInt32(carbonDioxideList[0], 2);
+Console.WriteLine($"Oxygen value is {carbonDioxideList[0]} or {carbonDioxideValue}");
+
+// Result
+Console.WriteLine($"Thus, the result is {oxygenValue * carbonDioxideValue}");
+
+
+/*****************************************\
+|*        Methods and classes            *|
+\*****************************************/
+
+Values CountAtLocation(IEnumerable<string> haystack, int index)
+{
+    var values = new Values();
+    foreach (var str in haystack)
+    {
+        if (str[index] == '0') values.Zeroes++;
+        if (str[index] == '1') values.Ones++;
+    }
+    return values;
+}
+
+internal class Values
+{
+    public int Zeroes { get; set; }
+    public int Ones { get; set; }
+}
